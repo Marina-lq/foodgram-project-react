c85cd4a46ae5ea400635d6a8c75528c929de6d4e
Marina Sudoplatova <97099985+Marina-lq@users.noreply.github.com>
2022-09-17 19:27:17 +0300

    08

--- backend/foodgram/recipes/management/commands/load_ingrs.py
+++ backend/foodgram/recipes/management/commands/load_ingrs.py
@@ -1,21 +1,24 @@
-import json
+import csv
 
 from django.core.management.base import BaseCommand
 
 from recipes.models import Ingredient
 
 
+SUCCESS_MSG = 'Ингредиенты выгружены.'
+FAILURE_MSG = 'Выгрузка ингредиентов прошла неудачно.'
+
+
 class Command(BaseCommand):
-    help = 'Load ingredients from json'
+    help = 'Загрузка ингредиентов из csv-файла'
 
-    def handle(self, *args, **options):
-        db_objects = []
-        with open('ingredients.json') as f:
-            data = json.load(f)
-
-            for ingredient_data in data:
-                db_objects.append(
-                    Ingredient(**ingredient_data)
-                )
-
-        Ingredient.objects.bulk_create(db_objects)
+    def handle(self, *args, **kwargs):
+        try:
+            with open('data/ingredients.csv', 'r', encoding='UTF-8') as file:
+                for row in csv.reader(file):
+                    Ingredient.objects.get_or_create(
+                        name=row[0], measurement_unit=row[1],
+                    )
+            self.stdout.write(self.style.SUCCESS(SUCCESS_MSG))
+        except Exception:
+            self.stdout.write(self.style.ERROR(FAILURE_MSG))
